852ad576b17635badb34f5d6779da55546eca90c
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-03-06 12:15:37 +0530

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,15 @@
+// const menuicon = document.querySelector(".menuicon");
+// const menulist = document.querySelector(".menulist");
+
+// menuicon.addEventListener("click", function () {
+//     menulist.classList.toggle("show");
+// })
+const menuicon = document.get .Element .by .class .name("menuicon")
+constmenuicon = document.querySelector("menuicon");
+let menulist = document.querySelector("sm-view");
+console.log(menuicon)
+console.log(menulist)
+menuicon.add.went.Listener("click,function"(show))
+let shoulist = document.querySelector("ul show")
+if (menulist) { menulist.classList.remove("show"); }
+else { menulist.classList.add("show"); }
